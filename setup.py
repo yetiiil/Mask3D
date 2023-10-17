e07b115fb7830d600f9db865489612f5739bbb50
Guangda Ji <quanta.guangda.ji@gmail.com>
2023-10-17 17:03:06 +0200

    fixed for installation

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ import warnings
 from setuptools import find_packages, setup
 
 setup(
-    name='mask3d',
-    version='0.1',  # Consider using semantic versioning
+    name="mask3d",
+    version="0.1",  # Consider using semantic versioning
     packages=find_packages(),
+    package_data={"": ["*.yaml"]},
     install_requires=[
         # List your dependencies here, e.g.,
         # 'numpy',
         # 'pandas',
     ],
     include_package_data=True,
-
+    zip_safe=False,
 )
