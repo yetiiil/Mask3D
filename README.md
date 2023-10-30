a62554a56e3d96e7df8eece8e230a8bf552f38b6
Silvan Weder <silvan.weder@inf.ethz.ch>
2023-10-30 13:51:51 +0100

    minimal example, fixed some issues

--- README.md
+++ README.md
@@ -44,16 +44,42 @@ pip install .
 To use the model in your code you need to download a checkpoint from the list below.
 Afterwards, the basic model can be used like:
 
+
 ```python
 from mask3d import get_model
 
-model = get_model()
+model = get_model(checkpoint_file='')
 ```
 
 
+Here is a minimal example assuming you have a pointcloud in the folder data.
 
+```python
 
+from mask3d import get_model, load_mesh, prepare_data, map_output_to_pointcloud, save_colorized_mesh 
 
+model = get_model('checkpoints/scannet200/scannet200_benchmark.ckpt')
+model.eval()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+# load input data
+pointcloud_file = 'data/pcl.ply'
+mesh = load_mesh(pointcloud_file)
+
+# prepare data
+data, points, colors, features, unique_map, inverse_map = prepare_data(mesh, device)
+
+# run model
+with torch.no_grad():
+    outputs = model(data, raw_coordinates=features)
+    
+# map output to point cloud
+labels = map_output_to_pointcloud(mesh, outputs, inverse_map)
+
+# save colorized mesh
+save_colorized_mesh(mesh, labels, 'data/pcl_labelled.ply', colormap='scannet200')
+```
 
 # Original Information
 
--- mask3d/__init__.py
+++ mask3d/__init__.py
@@ -22,28 +22,215 @@ import hydra
 from hydra.core.global_hydra import GlobalHydra
 from hydra.experimental import initialize, compose
 
-def get_model():
+# imports for input loading
+import albumentations as A
+import MinkowskiEngine as ME
+import numpy as np
+import open3d as o3d
+
+# imports for output
+from datasets.scannet200.scannet200_constants import (VALID_CLASS_IDS_20, VALID_CLASS_IDS_200, SCANNET_COLOR_MAP_20, SCANNET_COLOR_MAP_200)
+
+def get_model(checkpoint_path=None):
 
 
-  # Initialize the directory with config files
-  with initialize(config_path="conf"):
-      # Compose a configuration
-      cfg = compose(config_name="config_base_instance_segmentation.yaml")
-      print(OmegaConf.to_yaml(cfg))
+    # Initialize the directory with config files
+    with initialize(config_path="conf"):
+        # Compose a configuration
+        cfg = compose(config_name="config_base_instance_segmentation.yaml")
 
-  # # Initialize the Hydra context
-  # hydra.core.global_hydra.GlobalHydra.instance().clear()
-  # hydra.initialize(config_path="conf")
+    cfg.general.checkpoint = checkpoint_path
 
-  # Load the configuration
-  # cfg = hydra.compose(config_name="config_base_instance_segmentation.yaml")
-  model = InstanceSegmentation(cfg)
+    # would be nicd to avoid this hardcoding below
+    dataset_name = checkpoint_path.split('/')[-1].split('_')[0]
+    if dataset_name == 'scannet200':
+        cfg.general.num_targets = 201
+        cfg.general.train_mode = False
+        cfg.general.eval_on_segments = True
+        cfg.general.topk_per_image = 300
+        cfg.general.use_dbscan = True
+        cfg.general.dbscan_eps = 0.95
+        cfg.general.export_threshold = 0.001
 
-  if cfg.general.backbone_checkpoint is not None:
-      cfg, model = load_backbone_checkpoint_with_missing_or_exsessive_keys(
-          cfg, model
-      )
-  if cfg.general.checkpoint is not None:
-      cfg, model = load_checkpoint_with_missing_or_exsessive_keys(cfg, model)
+        # # data
+        cfg.data.num_labels = 200
+        cfg.data.test_mode = "test"
 
-  return model
+        # # model
+        cfg.model.num_queries = 150
+        
+    if dataset_name == 'scannet':
+        cfg.general.num_targets = 19
+        cfg.general.train_mode = False
+        cfg.general.eval_on_segments = True
+        cfg.general.topk_per_image = 300
+        cfg.general.use_dbscan = True
+        cfg.general.dbscan_eps = 0.95
+        cfg.general.export_threshold = 0.001
+
+        # # data
+        cfg.data.num_labels = 20
+        cfg.data.test_mode = "test"
+
+        # # model
+        cfg.model.num_queries = 150
+        
+        #TODO: this has to be fixed and discussed with Jonas
+        # cfg.model.scene_min = -3.
+        # cfg.model.scene_max = 3.
+
+    # # Initialize the Hydra context
+    # hydra.core.global_hydra.GlobalHydra.instance().clear()
+    # hydra.initialize(config_path="conf")
+
+    # Load the configuration
+    # cfg = hydra.compose(config_name="config_base_instance_segmentation.yaml")
+    model = InstanceSegmentation(cfg)
+
+    if cfg.general.backbone_checkpoint is not None:
+        cfg, model = load_backbone_checkpoint_with_missing_or_exsessive_keys(
+            cfg, model
+        )
+    if cfg.general.checkpoint is not None:
+        cfg, model = load_checkpoint_with_missing_or_exsessive_keys(cfg, model)
+
+    return model
+
+
+def load_mesh(pcl_file):
+    
+    # load point cloud
+    input_mesh_path = pcl_file
+    mesh = o3d.io.read_triangle_mesh(input_mesh_path)
+    return mesh
+
+def prepare_data(mesh, device):
+    
+    # normalization for point cloud features
+    color_mean = (0.47793125906962, 0.4303257521323044, 0.3749598901421883)
+    color_std = (0.2834475483823543, 0.27566157565723015, 0.27018971370874995)
+    normalize_color = A.Normalize(mean=color_mean, std=color_std)
+
+    
+    points = np.asarray(mesh.vertices)
+    colors = np.asarray(mesh.vertex_colors)
+    colors = colors * 255.
+
+    pseudo_image = colors.astype(np.uint8)[np.newaxis, :, :]
+    colors = np.squeeze(normalize_color(image=pseudo_image)["image"])
+
+    coords = np.floor(points / 0.02)
+    _, _, unique_map, inverse_map = ME.utils.sparse_quantize(
+        coordinates=coords,
+        features=colors,
+        return_index=True,
+        return_inverse=True,
+    )
+
+    sample_coordinates = coords[unique_map]
+    coordinates = [torch.from_numpy(sample_coordinates).int()]
+    sample_features = colors[unique_map]
+    features = [torch.from_numpy(sample_features).float()]
+
+    coordinates, _ = ME.utils.sparse_collate(coords=coordinates, feats=features)
+    features = torch.cat(features, dim=0)
+    data = ME.SparseTensor(
+        coordinates=coordinates,
+        features=features,
+        device=device,
+    )
+    
+    
+    return data, points, colors, features, unique_map, inverse_map
+
+
+def map_output_to_pointcloud(mesh, outputs, inverse_map, label_space='scannet200'):
+    
+    # parse predictions
+    logits = outputs["pred_logits"]
+    masks = outputs["pred_masks"]
+
+    # reformat predictions
+    logits = logits[0].detach().cpu()
+    masks = masks[0].detach().cpu()
+
+    labels = []
+    confidences = []
+    masks_binary = []
+
+    for i in range(len(logits)):
+        p_labels = torch.softmax(logits[i], dim=-1)
+        p_masks = torch.sigmoid(masks[:, i])
+        l = torch.argmax(p_labels, dim=-1)
+        c_label = torch.max(p_labels)
+        m = p_masks > 0.5
+        c_m = p_masks[m].sum() / (m.sum() + 1e-8)
+        c = c_label * c_m
+        if l < 200 and c > 0.5:
+            labels.append(l.item())
+            confidences.append(c.item())
+            masks_binary.append(
+                m[inverse_map])  # mapping the mask back to the original point cloud
+    
+    # save labelled mesh
+    mesh_labelled = o3d.geometry.TriangleMesh()
+    mesh_labelled.vertices = mesh.vertices
+    mesh_labelled.triangles = mesh.triangles
+
+    labels_mapped = np.zeros((len(mesh.vertices), 1))
+
+    for i, (l, c, m) in enumerate(
+        sorted(zip(labels, confidences, masks_binary), reverse=False)):
+        
+        if label_space == 'scannet200':
+            label_offset = 2
+            if l == 0:
+                l = -1 + label_offset
+            else:
+                l = int(l) + label_offset
+                        
+        labels_mapped[m == 1] = l
+        
+    return labels_mapped
+
+def save_colorized_mesh(mesh, labels_mapped, output_file, colormap='scannet'):
+    
+    # colorize mesh
+    colors = np.zeros((len(mesh.vertices), 3))
+    for li in np.unique(labels_mapped):
+        if colormap == 'scannet':
+            raise ValueError('Not implemented yet')
+        elif colormap == 'scannet200':
+            v_li = VALID_CLASS_IDS_200[int(li)]
+            colors[(labels_mapped == li)[:, 0], :] = SCANNET_COLOR_MAP_200[v_li]
+        else:
+            raise ValueError('Unknown colormap - not supported')
+    
+    colors = colors / 255.
+    mesh.vertex_colors = o3d.utility.Vector3dVector(colors)
+    o3d.io.write_triangle_mesh(output_file, mesh)
+
+if __name__ == '__main__':
+    
+    model = get_model('checkpoints/scannet200/scannet200_benchmark.ckpt')
+    model.eval()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    
+    # load input data
+    pointcloud_file = 'test_data/pcl.ply'
+    mesh = load_mesh(pointcloud_file)
+    
+    # prepare data
+    data, points, colors, features, unique_map, inverse_map = prepare_data(mesh, device)
+    
+    # run model
+    with torch.no_grad():
+        outputs = model(data, raw_coordinates=features)
+        
+    # map output to point cloud
+    labels = map_output_to_pointcloud(mesh, outputs, inverse_map)
+    
+    # save colorized mesh
+    save_colorized_mesh(mesh, labels, 'test_data/pcl_labelled.ply', colormap='scannet200')
+    
